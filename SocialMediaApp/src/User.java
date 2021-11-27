1614e170fff2a36a1520ff76c27d3f6495e116a0
szewan95 <szewanwong95@gmail.com>
2021-11-26 18:49:06 -0800

    Update User.java

--- SocialMediaApp/src/User.java
+++ SocialMediaApp/src/User.java
@@ -1,4 +1,18 @@
+import java.util.List;
 
 public class User {
-	
+	private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String userName;
+
+    private String password;
+    private String city;
+
+    private BST<User> friends;
+
+    private List<Interest> interests; //recommended to create an Interest class
 }
