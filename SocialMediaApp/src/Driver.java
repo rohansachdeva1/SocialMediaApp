f9ca4a1658dd6b0085f0fd83728aba3cf3c7bbcb
stevenwscss <stevenwscss@gmail.com>
2021-11-27 07:42:19 -0800

    Created driver class
    The driver class displays the menu and allows user to choose their operation

---
+++ SocialMediaApp/src/Driver.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Driver {
+	
+
+	public static void main(String[] args) {
+		boolean loginStatus = false;
+		int choice;
+		Scanner sc = new Scanner(System.in);
+		while (!loginStatus) {
+			System.out.println("Choose the following options ('1', '2' or '3'): ");
+			System.out.println("1: Login");
+			System.out.println("2: Create Account");
+			System.out.println("3: Exit");
+			choice = sc.nextInt();
+			switch(choice) {
+			case 1:
+//				login(); //might want to return to the main menu if login fail
+			case 2:
+//				createAccount(); //should create a new user
+				loginStatus = true;
+			}
+		}
+		while (loginStatus) {
+			System.out.println("Enter the following options ('1', '2' or '3'): ");
+			System.out.println("1: View my friends");
+			System.out.println("2: Make new friend");
+			System.out.println("3: Exit");
+			choice = sc.nextInt(); //could give some exception handlings
+			switch(choice) {
+			case 1:
+//				viewFriend();
+//				searchfriend();
+//				removefriend();
+//				goback();
+			case 2:
+//				makeFriend();
+//					searchbyname();
+//						displayProfile();
+//							addfriend() or goback();
+//					searchbyInterest();
+//						displayProfile();
+//							addfriend() or goback();
+//					getRecommendation();
+//						displayProfile();
+//							addfriend() or goback();
+//					goback();
+			case 3:
+//				Exit(); //save
+				loginStatus = false;
+			}
+			
+		}
+
+	}
+
+}
