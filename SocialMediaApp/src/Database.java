ba9b50b2196d4a796d4d42d8d88c3b241edd742a
stevenwscss <stevenwscss@gmail.com>
2021-11-26 19:02:20 -0800

    Adding BST class

---
+++ SocialMediaApp/src/Database.java
@@ -0,0 +1,7 @@
+	/**
+	 * This class load all the data from the data file and put them in appropriate data structures.
+	 * The data will be called in other class do do things like search, add friends, etc.
+	 */
+public class Database {
+
+}
