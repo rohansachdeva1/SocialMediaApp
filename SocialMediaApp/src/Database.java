cbcee098b87996645f187e48db9cc31fcccf5bf4
rohansachdeva1 <rohan.sachdeva1@gmail.com>
2021-11-29 20:51:24 -0500

    restructured database.java, still work in progress, need to work with hash team to figure out hashing of interests array list

--- SocialMediaApp/src/Database.java
+++ SocialMediaApp/src/Database.java
@@ -12,24 +12,45 @@ import Collections.*;
  * The data will be called in other class do do things like search, add friends, etc.
  */
 public class Database {
-	private static BST<User> allUsers;
-	private static int numOfUser;
+	private ArrayList<LinkedList<Integer>> allUsers; // graph database of all users
+	private static int numUsers; // holds number of users, used to create user id
+	private ArrayList<Integer> distance; // used in BFS to store distances from initial node
+	private ArrayList<Integer> interestScore; // array list of score calculated from # of interests in common between 2 users
+	private ArrayList<Integer> interests; // array list of interests and which users have them
+	private BST<User> userList = new BST<>();
 	
 	
 	// to load the data from a file
 	public Database() {
 		File file = new File("src/data.txt");
+
 		// create an arraylist to temporarily store the friend list, index 
-		ArrayList<LinkedList<Integer>> tempFriendList = new ArrayList<>();
-		tempFriendList.add(new LinkedList<Integer>());
+		//		ArrayList<LinkedList<Integer>> tempFriendList = new ArrayList<>();
+		//		tempFriendList.add(new LinkedList<Integer>());
+
+		allUsers = new ArrayList<>();
+		allUsers.add(new LinkedList<>()); // 0th linked list, not used
 		
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(file));
 			String stringFromDataTxt;
-			numOfUser = 0;
+			numUsers = 0;
+
+			/* INPUT FILE FORMAT FOR EACH USER
+			id
+			name
+			username
+			password
+			total number of friends
+			list of ids of all friends on separate lines
+			city
+			total number of interests
+			list of interests, each on a separate line
+			*/
+
+			// Loop through input file and take in all user data
 			while ((stringFromDataTxt = br.readLine()) != null) {
 				int userID = Integer.parseInt(stringFromDataTxt);
-				numOfUser++;
 				String fullName = br.readLine();
 				int spaceIndex = fullName.indexOf(" ");
 				String firstName = fullName.substring(0, spaceIndex);
@@ -39,29 +60,42 @@ public class Database {
 				String city = br.readLine();
 				int numOfFriends = Integer.parseInt(br.readLine());
 				
-
-				// BST friendBST = new BST();
-				tempFriendList.add(new LinkedList<Integer>());
+				// Add linked list for each new user
+				// 		tempFriendList.add(new LinkedList<Integer>());
+				allUsers.add(new LinkedList<>());
+				numUsers++;
 				
-				
+				// Loop through user's friend list and add to database
 				for (int i = 0; i < numOfFriends; i++) {
 					int friendID = Integer.parseInt(br.readLine());
-					tempFriendList.get(numOfUser).addLast(friendID);
+					allUsers.get(numUsers).addLast(friendID);
 				}
 
-				
+				// Create arraylist of interests
 				int numOfInterests = Integer.parseInt(br.readLine());
 				LinkedList<Interest> interestLinkedList = new LinkedList<>();
 				
+				// Loop through interests and add to arraylist
 				for (int j = 0; j < numOfInterests ; j++){
 					String interestName = br.readLine();
-					// FIXME: Use hashtable to search for interest object, if not exist, create one
-					Interest interestobj = new Interest();
-					interestLinkedList.addLast(interestobj); 
+					/*
+					HASH TEAM: Use a hashtable to search for interest object
+					need method that takes interestName as parameter and returns 
+					its postion in interests arraylist
+					*/ 
+					// need to figure out how to size interest arraylist when we do 
+					// not know the amount of interests from the start
+					int position = hash(interestName);
+
+					//Interest interestobj = new Interest();
+					//interestLinkedList.addLast(interestobj); 
+
+					interests.get(position).addLast(userID);
 				}
+
+				// Create new user from input data
 				User newUser = new User(userID, firstName, lastName, userName, password, city, interestLinkedList);
-				allUsers = new BST<User>();
-				allUsers.insert(newUser);
+				userList.insert(newUser);
 			}
 		
 		} catch (IOException e) {
